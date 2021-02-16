478c74a71e563482bb087f06863f76d450829af4
Ynda Jas <yndajas@gmail.com>
2021-02-16 11:30:18 +0000

    commit tree 919a42264d9690d057391cf74a1ff00325ee22be

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,17 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
+  def username
+    self.user.username
+  end
+
+  def post_title
+    self.post.title
+  end
+
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(user_attributes) unless user_attributes[:username].blank?
+  end
 end
